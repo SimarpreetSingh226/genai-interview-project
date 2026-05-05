bd28d33ea8cd9e9252e21178e313e359001e104f
SimarpreetSingh226 <ssimarpreet002@gmail.com>
2026-05-05 13:39:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://talentspark-ai-a6wd.onrender.com",
     credentials: true,
   }),
 );
